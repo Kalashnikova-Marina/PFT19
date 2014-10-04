c31e876eb3ded0eefbe3bb6428a7d9ac3cebd87a
Kalashnikova-Marina <kalashnikova.marina.amm@gmail.com>
2014-10-04 20:49:40 +0400

    Home task #13
    path in the script is absolute.

---
+++ Scripts/run_tests.bat
@@ -0,0 +1,3 @@
+cd ..\Addressbook-selenium-tests
+chcp 1251
+java -cp bin;D:\одр-19\Lib\xstream-1.4.7.jar;D:\одр-19\Lib\xstream\xpp3_min-1.1.4c.jar;D:\одр-19\Lib\selenium-server-standalone-2.43.0.jar;D:\одр-19\Lib\xstream\xmlpull-1.1.3.1.jar org.testng.TestNG testng-customsuite.xml
