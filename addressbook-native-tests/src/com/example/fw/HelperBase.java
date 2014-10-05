e77583be9d063d10a2a3b0bd3078752d16ba2e5e
Kalashnikova-Marina <kalashnikova.marina.amm@gmail.com>
2014-10-05 19:24:02 +0400

    Home task #18

---
+++ addressbook-native-tests/src/com/example/fw/HelperBase.java
@@ -0,0 +1,10 @@
+package com.example.fw;
+
+public class HelperBase {
+
+	protected final AppManager manager;
+	public HelperBase (AppManager applicationManager) {
+		this.manager = applicationManager;
+		
+	}
+}
